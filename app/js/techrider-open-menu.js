f69846c902355d08d6f922fbc3cedb495de6dece
Leonid <leonid.spb2015@yandex.ru>
2023-06-05 23:18:17 +0300

    Добавил табы на главной странице в блоке афишы

--- app/js/techrider-open-menu.js
+++ app/js/techrider-open-menu.js
@@ -1,54 +1,17 @@
-let openingMenu = document.querySelector('.opening-menu');
+let techriderOpeningMenu = document.querySelector('.techrider__opening-menu');
+let openingMenu = document.querySelectorAll('.opening-menu');
 let showedMenu = document.querySelectorAll('.showed-menu');
-const techriderOpeningMenu = document.querySelector('.techrider__opening-menu');
+
 
 
 function open() {
 	openingMenu.classList.toggle('opening-menu-active');
 	showedMenu.classList.toggle('showed-menu-active');
-
 }
 
+techriderOpeningMenu.addEventListener('click',open)
 
 
 
-addEventListener(`click`,e => {
-	let $target = e.target.closest(`.item-1`);
-	if ($target) {
-		$target.closest(`.opening-menu`).classList.toggle(`.opening-menu-active`);
-		return;
-	}
 
-	$target = e.target.closest(`.item-2`);
-	if ($target) {
-		$target.closest(`.opening-menu`).classList.toggle(`.opening-menu-active`);
-		return;
-	}
-	$target = e.target.closest(`.item-3`);
-	if ($target) {
-		$target.closest(`.opening-menu`).classList.toggle(`.opening-menu-active`);
-		return;
-	}
-	$target = e.target.closest(`.item-4`);
-	if ($target) {
-		$target.closest(`.opening-menu`).classList.toggle(`.opening-menu-active`);
-		return;
-	}
-	$target = e.target.closest(`.item-5`);
-	if ($target) {
-		$target.closest(`.opening-menu`).classList.toggle(`.opening-menu-active`);
-		return;
-	}
-	$target = e.target.closest(`.item-6`);
-	if ($target) {
-		$target.closest(`.opening-menu`).classList.toggle(`.opening-menu-active`);
-		return;
-	}
 
-});
-
-techriderOpeningMenu.addEventListener('click',(event) => {
-	if (event.target.closest('.opening-menu')) {
-		open();
-	}
-})
